b175223f7764e7639976cb9e428efb4d4a5c56a8
Eduardo Quezada <ing.eduardoq@gmail.com>
2026-02-01 12:51:04 -0400

    chore: format sec specs

--- tests/security.spec.ts
+++ tests/security.spec.ts
@@ -9,29 +9,29 @@ test.describe('Admin Users Page Tests', () => {
     await adminUsersPage.goto();
   });
 
-    /**
-   * @requirement Verify if we have access without login credentials
-   */
+  /**
+ * @requirement Verify if we have access without login credentials
+ */
   test('TC-SEC-08: Unauthorized URL Access', async () => {
     await expect(adminUsersPage.pageHeader).toHaveText('Users');
     await adminUsersPage.verifyTableHasData();
   });
 
 
-    /**
-   * @requirement Verify user details are exposed
-   */
+  /**
+ * @requirement Verify user details are exposed
+ */
   test('TC-SEC-09: Verify User Details', async () => {
     const user = await adminUsersPage.getUserDetailsByName('Leeroy Jenkins');
-    
+
     // Validate if data is vulnerable
     expect(user.name).toBe('Leeroy Jenkins');
     expect(user.password).toBe('VIZ10AWT8VL');
     expect(user.ssn).toBe('536-48-3769');
   });
-    /**
-   * @requirement Verify the URL access requires login credentials
-   */
+  /**
+ * @requirement Verify the URL access requires login credentials
+ */
   test('TC-SEC-10: Check Page Access', async () => {
     await adminUsersPage.navCurrencies.click();
     await expect(adminUsersPage.page).toHaveURL(/currencies.html/);
